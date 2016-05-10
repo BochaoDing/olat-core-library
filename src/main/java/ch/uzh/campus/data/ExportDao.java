634717851a2d297a3903b41c5e9aaa8c877ea6b5
OLAT Team <team@olat.org>
2016-05-10 11:07:12 +0200

    added Export to persistence.xml

--- src/main/java/ch/uzh/campus/data/ExportDao.java
+++ src/main/java/ch/uzh/campus/data/ExportDao.java
@@ -2,8 +2,6 @@ package ch.uzh.campus.data;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.olat.core.commons.persistence.DB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,16 +10,13 @@ import org.springframework.stereotype.Repository;
  * Initial Date: 04.06.2012 <br>
  * 
  * @author aabouc
+ * @author lavinia
  */
 @Repository
 public class ExportDao implements CampusDao<Export> {
 	@Autowired
     private DB dbInstance;
-
-    @PostConstruct
-    void initType() {
-        //genericDao.setType(Export.class);
-    }
+    
 
     @Override
     public void save(List<Export> exports) {
