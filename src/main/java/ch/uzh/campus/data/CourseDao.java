ef2204f0c58bb64c8c67538549dcf89e4c160766
OLAT Team <team@olat.org>
2016-05-11 13:42:14 +0200

    Added CourseDao.getCourseById() and CourseDaoTest

--- src/main/java/ch/uzh/campus/data/CourseDao.java
+++ src/main/java/ch/uzh/campus/data/CourseDao.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
  * 
  * @author aabouc
  * @author lavinia
+ * @author Martin Schraner
  */
 @Repository
 public class CourseDao implements CampusDao<Course> {
@@ -23,6 +24,10 @@ public class CourseDao implements CampusDao<Course> {
     	for (Course course : courses) {
             dbInstance.saveObject(course);
         }
+    }
+
+    public Course getCourseById(Long id) {
+        return dbInstance.findObject(Course.class, id);
     }
 
     public void addLecturerById(Long lecturerId, Long courseId) {
@@ -43,120 +48,118 @@ public class CourseDao implements CampusDao<Course> {
         }
     }
 
-    /*public Course updateCourse(Course course) {
-        return genericDao.update(course);
-    }
+//    public Course updateCourse(Course course) {
+//        return genericDao.update(course);
+//    }
+//
 
-    public Course getCourseById(Long id) {
-        return genericDao.findById(id);
-    }
 
-    public List<Course> getPilotCoursesByLecturerId(Long lecturerId) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("lecturerId", lecturerId);
-        return genericDao.getNamedQueryListResult(Course.GET_PILOT_COURSES_BY_LECTURER_ID, parameters);
-    }
+//    public List<Course> getPilotCoursesByLecturerId(Long lecturerId) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("lecturerId", lecturerId);
+//        return genericDao.getNamedQueryListResult(Course.GET_PILOT_COURSES_BY_LECTURER_ID, parameters);
+//    }
+//
+//    public List<Course> getCreatedCoursesByLecturerIds(List<Long> lecturerIds) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("lecturerIds", lecturerIds);
+//        return genericDao.getNamedQueryListResult(Course.GET_CREATED_COURSES_BY_LECTURER_IDS, parameters);
+//    }
+//
+//    public List<Course> getNotCreatedCoursesByLecturerIds(List<Long> lecturerIds) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("lecturerIds", lecturerIds);
+//        return genericDao.getNamedQueryListResult(Course.GET_NOT_CREATED_COURSES_BY_LECTURER_IDS, parameters);
+//    }
+//
+//    public List<Course> getPilotCoursesByStudentId(Long studentId) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("studentId", studentId);
+//        return genericDao.getNamedQueryListResult(Course.GET_PILOT_COURSES_BY_STUDENT_ID, parameters);
+//    }
+//
+//    public List<Course> getCreatedCoursesByStudentId(Long studentId) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("studentId", studentId);
+//        return genericDao.getNamedQueryListResult(Course.GET_CREATED_COURSES_BY_STUDENT_ID, parameters);
+//    }
+//
+//    public List<Course> getNotCreatedCoursesByStudentId(Long studentId) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("studentId", studentId);
+//        return genericDao.getNamedQueryListResult(Course.GET_NOT_CREATED_COURSES_BY_STUDENT_ID, parameters);
+//    }
+//
+//    public void delete(Course course) {
+//        genericDao.delete(course);
+//    }
+//
+//    public void saveResourceableId(Long courseId, Long resourceableId) {
+//        Query query = genericDao.getNamedQuery(Course.SAVE_RESOURCEABLE_ID);
+//        query.setParameter("courseId", courseId);
+//        query.setParameter("resId", resourceableId);
+//        query.executeUpdate();
+//    }
+//
+//    public void disableSynchronization(Long courseId) {
+//        Query query = genericDao.getNamedQuery(Course.DISABLE_SYNCHRONIZATION);
+//        query.setParameter("courseId", courseId);
+//        query.executeUpdate();
+//    }
+//
+//    public void deleteResourceableId(Long resourceableId) {
+//        Query query = genericDao.getNamedQuery(Course.DELETE_RESOURCEABLE_ID);
+//        query.setParameter("resId", resourceableId);
+//        query.executeUpdate();
+//    }
+//
+//    public void deleteByCourseId(Long courseId) {
+//        Query query = genericDao.getNamedQuery(Course.DELETE_BY_COURSE_ID);
+//        query.setParameter("courseId", courseId);
+//        query.executeUpdate();
+//    }
+//
+//    public void deleteByCourseIds(List<Long> courseIds) {
+//        Query query = genericDao.getNamedQuery(Course.DELETE_BY_COURSE_IDS);
+//        query.setParameterList("courseIds", courseIds);
+//        query.executeUpdate();
+//    }
+//
+//    public List<Long> getIdsOfAllCreatedCourses() {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        return genericDao.getNamedQueryEntityIds(Course.GET_IDS_OF_ALL_CREATED_COURSES, parameters);
+//    }
+//
+//    public List<Long> getResourceableIdsOfAllCreatedCourses() {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        return genericDao.getNamedQueryEntityIds(Course.GET_RESOURCEABLEIDS_OF_ALL_CREATED_COURSES, parameters);
+//    }
+//
+//    public List<Long> getIdsOfAllNotCreatedCourses() {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        return genericDao.getNamedQueryEntityIds(Course.GET_IDS_OF_ALL_NOT_CREATED_COURSES, parameters);
+//    }
+//
+//    public List<Course> getAllCreatedCourses() {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        return genericDao.getNamedQueryListResult(Course.GET_ALL_CREATED_COURSES, parameters);
+//    }
+//
+//    public List<Long> getAllNotUpdatedCourses(Date date) {
+//        Map<String, Object> parameters = new HashMap<String, Object>();
+//        parameters.put("lastImportDate", date);
+//        return genericDao.getNamedQueryEntityIds(Course.GET_ALL_NOT_UPDATED_COURSES, parameters);
+//    }
+//
+//    public boolean existResourceableId(Long resourceableId) {
+//        Map<String, Object> restrictionMap = new HashMap<String, Object>();
+//        restrictionMap.put("resourceableId", resourceableId);
+//        List<Course> courses = genericDao.findByCriteria(restrictionMap);
+//
+//        if (!courses.isEmpty()) {
+//            return true;
+//        }
+//        return false;
+//    }
 
-    public List<Course> getCreatedCoursesByLecturerIds(List<Long> lecturerIds) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("lecturerIds", lecturerIds);
-        return genericDao.getNamedQueryListResult(Course.GET_CREATED_COURSES_BY_LECTURER_IDS, parameters);
-    }
-
-    public List<Course> getNotCreatedCoursesByLecturerIds(List<Long> lecturerIds) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("lecturerIds", lecturerIds);
-        return genericDao.getNamedQueryListResult(Course.GET_NOT_CREATED_COURSES_BY_LECTURER_IDS, parameters);
-    }
-
-    public List<Course> getPilotCoursesByStudentId(Long studentId) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("studentId", studentId);
-        return genericDao.getNamedQueryListResult(Course.GET_PILOT_COURSES_BY_STUDENT_ID, parameters);
-    }
-
-    public List<Course> getCreatedCoursesByStudentId(Long studentId) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("studentId", studentId);
-        return genericDao.getNamedQueryListResult(Course.GET_CREATED_COURSES_BY_STUDENT_ID, parameters);
-    }
-
-    public List<Course> getNotCreatedCoursesByStudentId(Long studentId) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("studentId", studentId);
-        return genericDao.getNamedQueryListResult(Course.GET_NOT_CREATED_COURSES_BY_STUDENT_ID, parameters);
-    }
-
-    public void delete(Course course) {
-        genericDao.delete(course);
-    }
-
-    public void saveResourceableId(Long courseId, Long resourceableId) {
-        Query query = genericDao.getNamedQuery(Course.SAVE_RESOURCEABLE_ID);
-        query.setParameter("courseId", courseId);
-        query.setParameter("resId", resourceableId);
-        query.executeUpdate();
-    }
-
-    public void disableSynchronization(Long courseId) {
-        Query query = genericDao.getNamedQuery(Course.DISABLE_SYNCHRONIZATION);
-        query.setParameter("courseId", courseId);
-        query.executeUpdate();
-    }
-
-    public void deleteResourceableId(Long resourceableId) {
-        Query query = genericDao.getNamedQuery(Course.DELETE_RESOURCEABLE_ID);
-        query.setParameter("resId", resourceableId);
-        query.executeUpdate();
-    }
-
-    public void deleteByCourseId(Long courseId) {
-        Query query = genericDao.getNamedQuery(Course.DELETE_BY_COURSE_ID);
-        query.setParameter("courseId", courseId);
-        query.executeUpdate();
-    }
-
-    public void deleteByCourseIds(List<Long> courseIds) {
-        Query query = genericDao.getNamedQuery(Course.DELETE_BY_COURSE_IDS);
-        query.setParameterList("courseIds", courseIds);
-        query.executeUpdate();
-    }
-
-    public List<Long> getIdsOfAllCreatedCourses() {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        return genericDao.getNamedQueryEntityIds(Course.GET_IDS_OF_ALL_CREATED_COURSES, parameters);
-    }
-
-    public List<Long> getResourceableIdsOfAllCreatedCourses() {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        return genericDao.getNamedQueryEntityIds(Course.GET_RESOURCEABLEIDS_OF_ALL_CREATED_COURSES, parameters);
-    }
-
-    public List<Long> getIdsOfAllNotCreatedCourses() {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        return genericDao.getNamedQueryEntityIds(Course.GET_IDS_OF_ALL_NOT_CREATED_COURSES, parameters);
-    }
-
-    public List<Course> getAllCreatedCourses() {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        return genericDao.getNamedQueryListResult(Course.GET_ALL_CREATED_COURSES, parameters);
-    }
-
-    public List<Long> getAllNotUpdatedCourses(Date date) {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("lastImportDate", date);
-        return genericDao.getNamedQueryEntityIds(Course.GET_ALL_NOT_UPDATED_COURSES, parameters);
-    }
-
-    public boolean existResourceableId(Long resourceableId) {
-        Map<String, Object> restrictionMap = new HashMap<String, Object>();
-        restrictionMap.put("resourceableId", resourceableId);
-        List<Course> courses = genericDao.findByCriteria(restrictionMap);
-
-        if (!courses.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
-*/
 }
---
+++ src/test/java/ch/uzh/campus/data/CourseDaoTest.java
@@ -0,0 +1,176 @@
+package ch.uzh.campus.data;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.olat.core.commons.persistence.DB;
+import org.olat.test.OlatTestCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.inject.Provider;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Initial Date: Oct 28, 2014 <br>
+ *
+ * @author aabouc
+ * @author Martin Schraner
+ */
+@ContextConfiguration(locations = {"classpath:ch/uzh/campus/data/_spring/mockDataContext.xml"})
+public class CourseDaoTest extends OlatTestCase {
+
+    @Autowired
+    private DB dbInstance;
+
+    @Autowired
+    private CourseDao courseDao;
+
+    @Autowired
+    private Provider<MockDataGenerator> mockDataGeneratorProvider;
+
+    private List<Course> courses;
+
+    @Before
+    public void setup() {
+        courses = mockDataGeneratorProvider.get().getCourses();
+        courseDao.save(courses);
+        dbInstance.flush();
+    }
+
+    @After
+    public void after() {
+        dbInstance.rollback();
+    }
+
+    @Test
+    public void testNullGetCourseById() {
+        assertNull(courseDao.getCourseById(999L));
+    }
+
+    @Test
+    public void testNotNullGetCourseById() {
+        assertNotNull(courseDao.getCourseById(100L));
+    }
+
+//    @Test
+//    public void testDelete() {
+//        assertNotNull(courseDao.getCourseById(100L));
+//        courseDao.delete(courses.get(0));
+//        assertNull(courseDao.getCourseById(100L));
+//    }
+//
+//    @Test
+//    public void testDeleteByCourseId() {
+//        assertNotNull(courseDao.getCourseById(100L));
+//        courseDao.deleteByCourseId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        assertNull(courseDao.getCourseById(100L));
+//    }
+//
+//    @Test
+//    public void testSaveResourceableId() {
+//        Course course = courseDao.getCourseById(100L);
+//        assertEquals(course.getResourceableId().longValue(), 100L);
+//        courseDao.saveResourceableId(100L, 1000L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        Course updatedCourse = courseDao.getCourseById(100L);
+//        assertEquals(updatedCourse.getResourceableId().longValue(), 1000L);
+//    }
+//
+//    @Test
+//    public void testDisableSynchronization() {
+//        Course course = courseDao.getCourseById(100L);
+//        assertTrue(course.isSynchronizable());
+//        courseDao.disableSynchronization(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        Course updatedCourse = courseDao.getCourseById(100L);
+//        assertFalse(updatedCourse.isSynchronizable());
+//    }
+//
+//    @Test
+//    public void testDeleteResourceableId() {
+//        Course course = courseDao.getCourseById(100L);
+//        assertEquals(course.getResourceableId().longValue(), 100L);
+//        courseDao.deleteResourceableId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        Course updatedCourse = courseDao.getCourseById(100L);
+//        assertNull(updatedCourse.getResourceableId());
+//    }
+//
+//    @Test
+//    public void testDeleteByCourseIds() {
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 2);
+//        List<Long> courseIds = new LinkedList<Long>();
+//        courseIds.add(100L);
+//        courseIds.add(200L);
+//        courseDao.deleteByCourseIds(courseIds);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 0);
+//    }
+//
+//    @Test
+//    public void testGetResourceableIdsOfAllCreatedCourses() {
+//        List<Long> resourceableIdsOfAllCreatedCourses = courseDao.getResourceableIdsOfAllCreatedCourses();
+//        assertTrue(resourceableIdsOfAllCreatedCourses.contains(100L));
+//        assertFalse(resourceableIdsOfAllCreatedCourses.contains(999L));
+//    }
+//
+//    @Test
+//    public void testGetIdsOfAllCreatedCourses() {
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 2);
+//        courseDao.deleteResourceableId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 1);
+//    }
+//
+//    @Test
+//    public void testGetIdsOfAllNotCreatedCourses() {
+//        assertEquals(courseDao.getIdsOfAllNotCreatedCourses().size(), 0);
+//        courseDao.deleteResourceableId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        assertEquals(courseDao.getIdsOfAllNotCreatedCourses().size(), 1);
+//    }
+//
+//    @Test
+//    public void testGetAllCreatedCourses() {
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 2);
+//        courseDao.deleteResourceableId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//        assertEquals(courseDao.getAllCreatedCourses().size(), 1);
+//    }
+//
+//    @Test
+//    public void tesGetAllNotUpdatedCourses() {
+//        courseDao.deleteResourceableId(100L);
+//        courseDao.deleteResourceableId(200L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//
+//        assertEquals(courseDao.getAllNotUpdatedCourses(new Date()).size(), 2);
+//
+//        Date now = new Date();
+//
+//        courses.get(0).setModifiedDate(now);
+//        courseDao.saveOrUpdate(courses);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//
+//        assertEquals(courseDao.getAllNotUpdatedCourses(new Date()).size(), 1);
+//    }
+//
+//    @Test
+//    public void testExistResourceableId() {
+//        assertTrue(courseDao.existResourceableId(100L));
+//
+//        courseDao.deleteResourceableId(100L);
+//        transactionManager.getSessionFactory().getCurrentSession().clear();
+//
+//        assertFalse(courseDao.existResourceableId(100L));
+//
+//    }
+
+
+}
