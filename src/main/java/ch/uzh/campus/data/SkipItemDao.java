c4c79be5bae13c80e51e177da444b2377cd86a0e
OLAT Team <team@olat.org>
2016-05-17 19:40:04 +0200

    reintroduce SkipItemDao

---
+++ src/main/java/ch/uzh/campus/data/SkipItemDao.java
@@ -0,0 +1,27 @@
+package ch.uzh.campus.data;
+
+import org.olat.core.commons.persistence.DB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Repository
+public class SkipItemDao implements CampusDao<SkipItem> {
+
+    @Autowired
+    private DB dbInstance;
+
+    @Override
+    public void save(List<SkipItem> skipItems) {
+        for (SkipItem skipItem : skipItems) {
+            dbInstance.saveObject(skipItem);
+        }
+    }
+
+    public void save(SkipItem skipItem) {
+        dbInstance.saveObject(skipItem);
+    }
+
+}
