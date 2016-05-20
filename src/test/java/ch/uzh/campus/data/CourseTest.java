709d63cf5eadbe043d345624705fc79d8ce4d4cd
OLAT Team <team@olat.org>
2016-05-20 18:49:11 +0200

    added CourseTest, fixed ImportStatisticDaoTest

---
+++ src/test/java/ch/uzh/campus/data/CourseTest.java
@@ -0,0 +1,56 @@
+package ch.uzh.campus.data;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by Martin Schraner on 02.03.16.
+ */
+public class CourseTest {
+
+    private Course course = new Course();
+
+    @Test
+    public void testGetTitleToBeDisplayed() throws Exception {
+        course.setTitle("Community Ecology");
+        course.setShortSemester("16FS");
+        course.setShortTitle("07BKECO339");
+        assertEquals("16FS ECO339 Community Ecology", course.getTitleToBeDisplayed("true"));
+    }
+
+    @Test
+    public void testGetTitleToBeDisplayed_shortTitle_notActivated() throws Exception {
+        course.setTitle("Community Ecology");
+        course.setShortSemester("16FS");
+        course.setShortTitle("07BKECO339");
+        assertEquals("16FS Community Ecology", course.getTitleToBeDisplayed("false"));
+    }
+
+    @Test
+    public void testGetTitleToBeDisplayed_shortTitle_notActivated_missingProperty() throws Exception {
+        course.setTitle("Community Ecology");
+        course.setShortSemester("16FS");
+        course.setShortTitle("07BKECO339");
+        assertEquals("16FS Community Ecology", course.getTitleToBeDisplayed(null));
+    }
+
+    @Test
+    public void testGetTitleToBeDisplayed_shortTitle_notActivated_emptyProperty() throws Exception {
+        Course course = new Course();
+        course.setTitle("Community Ecology");
+        course.setShortSemester("16FS");
+        course.setShortTitle("07BKECO339");
+        assertEquals("16FS Community Ecology", course.getTitleToBeDisplayed(""));
+    }
+
+    @Test
+    public void testGetTitleToBeDisplayed_semesterIsNull_shortTitleIsNull() throws Exception {
+        course.setTitle("Community Ecology");
+        course.setShortSemester(null);
+        course.setShortTitle(null);
+        assertEquals("Community Ecology", course.getTitleToBeDisplayed("true"));
+    }
+}
+
