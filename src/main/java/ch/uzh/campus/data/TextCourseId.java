056abee952ae4edf8c5d3a8aa411df04a28164e5
OLAT Team <team@olat.org>
2016-06-01 10:40:11 +0200

    serialization code for TextCourseId

--- src/main/java/ch/uzh/campus/data/TextCourseId.java
+++ src/main/java/ch/uzh/campus/data/TextCourseId.java
@@ -1,6 +1,7 @@
 package ch.uzh.campus.data;
 
 
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class TextCourseId extends Text {
 
@@ -12,5 +13,38 @@ public class TextCourseId extends Text {
 
     public void setCourseId(Long courseId) {
         this.courseId = courseId;
+    }
+
+    @Override
+    public String toString() {
+        ToStringBuilder builder = new ToStringBuilder(this);
+        builder.append("id", getId());
+        builder.append("courseId", getCourseId());
+        builder.append("type", getType());
+        builder.append("lineSeq", getLineSeq());
+        builder.append("line", getLine());
+
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Text text = (Text) o;
+
+        if (getLineSeq() != text.getLineSeq()) return false;
+        if (!getType().equals(text.getType())) return false;
+        return getLine().equals(text.getLine());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getType().hashCode();
+        result = 31 * result + getLineSeq();
+        result = 31 * result + getLine().hashCode();
+        return result;
     }
 }
