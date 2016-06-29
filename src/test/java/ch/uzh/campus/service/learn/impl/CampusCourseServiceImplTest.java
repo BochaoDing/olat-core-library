fd5d807e8c50d61ee65c4dc5c033c984ebefc810
OLAT Team <team@olat.org>
2016-06-29 16:45:51 +0200

    OLATNG-132 implemented missing tests

--- src/test/java/ch/uzh/campus/service/learn/impl/CampusCourseServiceImplTest.java
+++ src/test/java/ch/uzh/campus/service/learn/impl/CampusCourseServiceImplTest.java
@@ -42,12 +42,16 @@ public class CampusCourseServiceImplTest {
         Course course1 = new Course();
         course1.setId(new Long(50550670));
         course1.setTitle("Wahlpflichtmodul MA: Forschungsseminar Policy Analyse, 2-sem. (SA)");
+        course1.setShortTitle("Short Title 1");
+        course1.setVstNr("111111");
         coursesWithoutResourceableId.add(course1);
 
         // Course which could be opened
         Course course2 = new Course();
         course2.setId(new Long(50541483));
         course2.setTitle("English Literature: Textual Analysis, Part II (Vorlesung zum Seminar)");
+        course2.setShortTitle("Short Title 2");
+        course2.setVstNr("2222222");
         course2.setResourceableId(new Long(99999));
         coursesWithResourceableId.add(course2);
 
@@ -55,19 +59,26 @@ public class CampusCourseServiceImplTest {
         Course course3 = new Course();
         course3.setId(new Long(50541762));
         course3.setTitle("Pflichtmodul M.A.-Seminar Sprachwissenschaft (6 KP)");
+        course3.setShortTitle("Short Title 3");
+        course3.setVstNr("333333");
         coursesWithoutResourceableId.add(course3);
 
         // Mock for Identity
         identityMock = mock(Identity.class);
-        // when(usesrServiceMock.getUserProperty(identityMock, propertyName))
-        when(campusCourseCoreServiceMock.getCampusCoursesWithoutResourceableId(any(Identity.class), SapOlatUser.SapUserType.LECTURER)).thenReturn(
-                coursesWithoutResourceableId);
 
+        when(campusCourseCoreServiceMock.getCampusCoursesWithoutResourceableId(identityMock, SapOlatUser.SapUserType.LECTURER, null))
+                .thenReturn(coursesWithoutResourceableId);
+        when(campusCourseCoreServiceMock.getCampusCoursesWithResourceableId(identityMock, SapOlatUser.SapUserType.LECTURER, null))
+                .thenReturn(coursesWithResourceableId);
     }
 
-    @Ignore
+    @Test
+    public void getCoursesWhichCouldBeOpened() {
+        assertTrue(campusCourseLearnServiceImplTestObject.getCoursesWhichCouldBeOpened(identityMock, SapOlatUser.SapUserType.LECTURER, null).size() == coursesWithResourceableId.size());
+    }
+
     @Test
     public void getCoursesWhichCouldBeCreated() {
-        assertTrue(campusCourseLearnServiceImplTestObject.getCoursesWhichCouldBeCreated(identityMock, SapOlatUser.SapUserType.LECTURER, null).size() == 1);
+        assertTrue(campusCourseLearnServiceImplTestObject.getCoursesWhichCouldBeCreated(identityMock, SapOlatUser.SapUserType.LECTURER, null).size() == coursesWithoutResourceableId.size());
     }
 }
