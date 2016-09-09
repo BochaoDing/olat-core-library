713a8dd15f6d2bfe78f5cc1a207866241df53868
frentix GmbH <contact@frentix.com>
2016-09-09 13:48:16 +0200

    OO-2175: reset paging on extended search

--- src/main/java/org/olat/core/gui/components/form/flexible/impl/elements/table/FlexiTableElementImpl.java
+++ src/main/java/org/olat/core/gui/components/form/flexible/impl/elements/table/FlexiTableElementImpl.java
@@ -1306,7 +1306,20 @@ public class FlexiTableElementImpl extends FormItemImpl implements FlexiTableEle
 		resetInternComponents();
 		reloadData();
 	}
-	
+
+	@Override
+	public void reset(boolean page, boolean internal, boolean reloadData) {
+		if(page) {
+			currentPage = 0;
+		}
+		if(internal) {
+			resetInternComponents();
+		}
+		if(reloadData) {
+			reloadData();
+		}
+	}
+
 	private void resetInternComponents() {
 		rowCount = -1;
 		component.setDirty(true);
