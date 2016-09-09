fdee0ccc62b16386ddd3e1d2f5049d839969ac87
OLAT Team <team@olat.org>
2016-09-09 16:18:21 +0200

    LMSUZH-57: Replaced powersearch in user mapping by own query

--- src/main/java/ch/uzh/campus/mapper/UserMapper.java
+++ src/main/java/ch/uzh/campus/mapper/UserMapper.java
@@ -2,43 +2,21 @@ package ch.uzh.campus.mapper;
 
 import ch.uzh.campus.CampusCourseConfiguration;
 import org.apache.commons.lang.StringUtils;
-import org.olat.basesecurity.BaseSecurity;
 import org.olat.core.commons.persistence.DB;
 import org.olat.core.id.Identity;
 import org.olat.core.id.UserConstants;
 import org.olat.core.logging.OLog;
 import org.olat.core.logging.Tracing;
+import org.olat.user.UserImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.TypedQuery;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
- * OLAT - Online Learning and Training<br>
- * http://www.olat.org
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); <br>
- * you may not use this file except in compliance with the License.<br>
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing,<br>
- * software distributed under the License is distributed on an "AS IS" BASIS, <br>
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
- * See the License for the specific language governing permissions and <br>
- * limitations under the License.
- * <p>
- * Copyright (c) since 2004 at Multimedia- & E-Learning Services (MELS),<br>
- * University of Zurich, Switzerland.
- * <p>
- *
- * Initial Date: 28.06.2012 <br>
- * 
- * @author cg
  * @author Martin Schraner
  */
 @Component
@@ -48,13 +26,11 @@ public class UserMapper {
 
     private final DB dbInstance;
     private final CampusCourseConfiguration campusCourseConfiguration;
-    private final BaseSecurity baseSecurity;
 
     @Autowired
-    public UserMapper(DB dbInstance, CampusCourseConfiguration campusCourseConfiguration, BaseSecurity baseSecurity) {
+    public UserMapper(DB dbInstance, CampusCourseConfiguration campusCourseConfiguration) {
         this.dbInstance = dbInstance;
         this.campusCourseConfiguration = campusCourseConfiguration;
-        this.baseSecurity = baseSecurity;
     }
 
     Identity tryToMapByMatriculationNumber(String matriculationNumber) {
@@ -76,11 +52,12 @@ public class UserMapper {
             return null;
         }
 
-        // Perform expensive power search in case we found someone
-        HashMap<String, String> userProperties = new HashMap<>();
-        userProperties.put(UserConstants.INSTITUTIONAL_MATRICULATION_NUMBER, matriculationNumberDigitsOnly);
-        userProperties.put(UserConstants.INSTITUTIONALNAME, campusCourseConfiguration.getMappingInstitutionalName());
-        return findIdentityByPowerSearch(userProperties);
+        // Execute more expensive query to find identity in case we found someone
+        String multipleIdentitiesFoundWarning = "Tried to map by " + UserConstants.INSTITUTIONAL_MATRICULATION_NUMBER + " = " + matriculationNumberDigitsOnly
+                + ", " + UserConstants.INSTITUTIONALNAME + " = " + campusCourseConfiguration.getMappingInstitutionalName()
+                + " and found more than one matching identity.";
+
+        return findIdentity(userIdsFound, multipleIdentitiesFoundWarning);
     }
 
     Identity tryToMapByPersonalNumber(Long employeeNumber) {
@@ -103,25 +80,37 @@ public class UserMapper {
             return null;
         }
 
-        // Perform expensive power search in case we found someone
-        HashMap<String, String> userProperties = new HashMap<>();
-        userProperties.put(UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER, employeeNumberAllowLeadingZeroes);
-        userProperties.put(UserConstants.INSTITUTIONALNAME, campusCourseConfiguration.getMappingInstitutionalName());
-        Identity mappedIdentity = findIdentityByPowerSearch(userProperties);
-
         // We have to check that we only found personal numbers with leading ZEROES with the "like" in the query above
-        if (mappedIdentity != null) {
-            String employeeNumberOfUserProperty = mappedIdentity.getUser().getProperty(UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER, null);
-            try {
-                Long employeeNumberOfUserPropertyAsLong = Long.valueOf(employeeNumberOfUserProperty);
-                if (employeeNumberOfUserPropertyAsLong.equals(employeeNumber)) {
-                    return mappedIdentity;
+        // -> remove userId from list if it does not match
+        Iterator<Long> iterator = userIdsFound.iterator();
+        while (iterator.hasNext()) {
+            Long userId = iterator.next();
+            UserImpl user = dbInstance.getCurrentEntityManager().find(UserImpl.class, userId);
+            if (user != null) {
+                String employeeNumberOfUserProperty = user.getProperty(UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER, null);
+                try {
+                    Long employeeNumberOfUserPropertyAsLong = Long.valueOf(employeeNumberOfUserProperty);
+                    if (employeeNumberOfUserPropertyAsLong.equals(employeeNumber)) {
+                        // everything fine
+                        continue;
+                    }
+                } catch (NumberFormatException ex) {
+                    LOG.warn("Could not convert personal-number " + employeeNumberOfUserProperty + " to Long");
                 }
-            } catch (NumberFormatException ex) {
-                LOG.warn("Could not convert personal-number " + employeeNumberOfUserProperty + " to Long");
             }
+            iterator.remove();
         }
-        return null;
+
+        if (userIdsFound.isEmpty()) {
+            return null;
+        }
+
+        // Execute more expensive query to find identity in case we found someone
+        String multipleIdentitiesFoundWarning = "Tried to map by " + UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER + " = " + employeeNumberAllowLeadingZeroes
+                + ", " + UserConstants.INSTITUTIONALNAME + " = " + campusCourseConfiguration.getMappingInstitutionalName()
+                + " and found more than one matching identity.";
+
+        return findIdentity(userIdsFound, multipleIdentitiesFoundWarning);
     }
 
     Identity tryToMapByAdditionalPersonalNumber(String additionalEmployeeNumber) {
@@ -141,11 +130,12 @@ public class UserMapper {
             return null;
         }
 
-        // Perform expensive power search in case we found someone
-        HashMap<String, String> userProperties = new HashMap<>();
-        userProperties.put(UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER, additionalEmployeeNumber);
-        userProperties.put(UserConstants.INSTITUTIONALNAME, campusCourseConfiguration.getMappingInstitutionalName());
-        return findIdentityByPowerSearch(userProperties);
+        // Execute more expensive query to find identity in case we found someone
+        String multipleIdentitiesFoundWarning = "Tried to map by " + UserConstants.INSTITUTIONAL_EMPLOYEE_NUMBER + " = " + additionalEmployeeNumber
+                + ", " + UserConstants.INSTITUTIONALNAME + " = " + campusCourseConfiguration.getMappingInstitutionalName()
+                + " and found more than one matching identity.";
+
+        return findIdentity(userIdsFound, multipleIdentitiesFoundWarning);
     }
 
     Identity tryToMapByEmail(String email) {
@@ -154,7 +144,7 @@ public class UserMapper {
             return null;
         }
 
-        // As a first check, execute cheap query
+        // Execute cheap query to find user id
         TypedQuery<Long> query = dbInstance.getCurrentEntityManager().createQuery("select up1.propertyId.userId from userproperty up1 where " +
                 "up1.propertyId.name = :propname1 and up1.value = :propvalue1 " +
                 "and exists (select up2.propertyId.userId from userproperty up2 where up1.propertyId.userId = up2.propertyId.userId and up2.propertyId.name = :propname2 and up2.value = :propvalue2)", Long.class);
@@ -170,11 +160,12 @@ public class UserMapper {
             return null;
         }
 
-        // Perform expensive power search in case we found someone
-        HashMap<String, String> userProperties = new HashMap<>();
-        userProperties.put(UserConstants.EMAIL, email);
-        userProperties.put(UserConstants.INSTITUTIONALNAME, campusCourseConfiguration.getMappingInstitutionalName());
-        return findIdentityByPowerSearch(userProperties);
+        // Execute more expensive query to find identity in case we found someone
+        String multipleIdentitiesFoundWarning = "Tried to map by " + UserConstants.EMAIL + " = " + email
+                + ", " + UserConstants.INSTITUTIONALNAME + " = " + campusCourseConfiguration.getMappingInstitutionalName()
+                + " and found more than one matching identity.";
+
+        return findIdentity(userIdsFound, multipleIdentitiesFoundWarning);
     }
 
     Identity tryToMapByFirstNameLastName(String firstName, String lastName) {
@@ -197,33 +188,38 @@ public class UserMapper {
             return null;
         }
 
-        // Perform expensive power search in case we found someone
-        HashMap<String, String> userProperties = new HashMap<>();
-        userProperties.put(UserConstants.FIRSTNAME, firstName);
-        userProperties.put(UserConstants.LASTNAME, lastName);
-        userProperties.put(UserConstants.INSTITUTIONALNAME, campusCourseConfiguration.getMappingInstitutionalName());
-        return findIdentityByPowerSearch(userProperties);
+        // Execute more expensive query to find identity in case we found someone
+        String multipleIdentitiesFoundWarning = "Tried to map by " + UserConstants.FIRSTNAME + " = " + firstName
+                + ", " + UserConstants.LASTNAME + " = " + lastName
+                + ", " + UserConstants.INSTITUTIONALNAME + " = " + campusCourseConfiguration.getMappingInstitutionalName()
+                + " and found more than one matching identity.";
+
+        return findIdentity(userIdsFound, multipleIdentitiesFoundWarning);
     }
 
-    private Identity findIdentityByPowerSearch(Map<String, String> userProperties) {
+    private Identity findIdentity(List<Long> userIds, String multipleIdentitiesFoundWarning) {
 
-        List<Identity> visibleIdentitiesFound = baseSecurity.getVisibleIdentitiesByPowerSearch(null, userProperties, true, null, null, null, null, null);
+        List<UserImpl> users = new ArrayList<>();
+        for (Long userId : userIds) {
+            users.add(dbInstance.getCurrentEntityManager().getReference(UserImpl.class, userId));
+        }
 
-        if (visibleIdentitiesFound.isEmpty()) {
+        // Identity must be visible
+        @SuppressWarnings("JpaQlInspection")
+        TypedQuery<Identity> query = dbInstance.getCurrentEntityManager().createQuery("select ident from org.olat.core.id.Identity as ident where " +
+                "ident.user in :users and ident.status < :status", Identity.class);
+
+        List<Identity> identitiesFound = query.setParameter("users", users)
+                .setParameter("status", Identity.STATUS_VISIBLE_LIMIT)
+                .getResultList();
+
+        if (identitiesFound.isEmpty()) {
             return null;
-        } else if (visibleIdentitiesFound.size() == 1) {
-            return visibleIdentitiesFound.get(0);
+        } else if (identitiesFound.size() == 1) {
+            return identitiesFound.get(0);
         } else {
-            StringBuilder warningMsgSb = new StringBuilder("Tried to map by ");
-            for (Map.Entry<String, String> entry : userProperties.entrySet()) {
-                warningMsgSb.append(entry.getKey()).append(" = ").append(entry.getValue()).append(", ");
-            }
-            // Remove last ", "
-            warningMsgSb.setLength(warningMsgSb.length() - 2);
-            warningMsgSb.append(" and found more than one matching identity.");
-            LOG.warn(warningMsgSb.toString());
+            LOG.warn(multipleIdentitiesFoundWarning);
             return null;
         }
     }
-
 }
