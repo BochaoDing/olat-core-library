2701e2bf9097d584c9a3da2bdb2388217f29f5dc
OLAT Team <team@olat.org>
2016-10-04 13:18:11 +0200

    Simplified OLATResourceManager.findResourceById() by using JPA find() method

--- src/main/java/org/olat/resource/OLATResourceManager.java
+++ src/main/java/org/olat/resource/OLATResourceManager.java
@@ -215,13 +215,7 @@ public class OLATResourceManager extends BasicManager {
 	
 	public OLATResource findResourceById(Long key) {
 		if (key == null) return null;
-
-		String s = "select ori from org.olat.resource.OLATResourceImpl ori where ori.key=:resourceKey";
-		List<OLATResource> resources = dbInstance.getCurrentEntityManager()
-				.createQuery(s, OLATResource.class)
-				.setParameter("resourceKey", key)
-				.getResultList();
-		return resources.isEmpty() ? null : resources.get(0);
+		return dbInstance.getCurrentEntityManager().find(OLATResourceImpl.class, key);
 	}
 	
 	public List<OLATResource> findResourceByTypes(List<String> types) {
