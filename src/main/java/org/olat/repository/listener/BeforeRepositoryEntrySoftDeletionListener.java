e84314ca2a451230c0b71b5f2e97a126a9031c47
OLAT Team <team@olat.org>
2017-07-31 15:14:33 +0200

    Avoid that continued campus courses can be deleted

--- src/main/java/org/olat/repository/listener/BeforeRepositoryEntrySoftDeletionListener.java
+++ src/main/java/org/olat/repository/listener/BeforeRepositoryEntrySoftDeletionListener.java
@@ -2,22 +2,21 @@ package org.olat.repository.listener;
 
 import org.olat.repository.RepositoryEntry;
 import org.olat.repository.manager.RepositoryEntryDeletionException;
-import org.olat.resource.OLATResource;
 import org.springframework.stereotype.Component;
 
 /**
  * In order the event listener array is never null, one listener must exist.
  * Therefore this listener is implemented as class.
  *
- * Initial date: 2016-07-28<br />
+ * Initial date: 2017-07-31<br />
  * @author Martin Schraner
  */
 @Component
-public class BeforeRepositoryEntryPermanentDeletionListener {
+public class BeforeRepositoryEntrySoftDeletionListener {
 
 	/**
 	 * This method should never commit the running database transaction.
 	 */
-	public void onAction(RepositoryEntry repositoryEntry, OLATResource resource) throws RepositoryEntryDeletionException {
+	public void onAction(RepositoryEntry repositoryEntry) throws RepositoryEntryDeletionException {
 	}
 }
