60032bcf20ee27d7a4c60a697458cb2a37909d75
OLAT Team <team@olat.org>
2018-08-08 18:12:19 +0200

    alternative exporter for group members - for many groups at once, with extra column for group name

--- src/main/java/org/olat/commons/memberlist/manager/XlsGroupMembersExport.java
+++ src/main/java/org/olat/commons/memberlist/manager/XlsGroupMembersExport.java
@@ -1,29 +1,4 @@
-/**
- * <a href="http://www.openolat.org">
- * OpenOLAT - Online Learning and Training</a><br>
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License"); <br>
- * you may not use this file except in compliance with the License.<br>
- * You may obtain a copy of the License at the
- * <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache homepage</a>
- * <p>
- * Unless required by applicable law or agreed to in writing,<br>
- * software distributed under the License is distributed on an "AS IS" BASIS, <br>
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. <br>
- * See the License for the specific language governing permissions and <br>
- * limitations under the License.
- * <p>
- * Initial code contributed and copyrighted by<br>
- * frentix GmbH, http://www.frentix.com
- * <p>
- */
 package org.olat.commons.memberlist.manager;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 import org.olat.admin.landingpages.ui.RulesDataModel;
 import org.olat.core.gui.media.MediaResource;
@@ -37,30 +12,40 @@ import org.olat.core.util.Util;
 import org.olat.core.util.openxml.OpenXMLWorkbook;
 import org.olat.core.util.openxml.OpenXMLWorkbookResource;
 import org.olat.core.util.openxml.OpenXMLWorksheet;
-import org.olat.core.util.openxml.OpenXMLWorksheet.Row;
+import org.olat.group.BusinessGroup;
 import org.olat.user.propertyhandlers.UserPropertyHandler;
 
-/**
- * Initial Date: 23.03.2017
- * @author fkiefer, fabian.kiefer@frentix.com, www.frentix.com
- */
-public class XlsMembersExport {
-	
-	private static final OLog log = Tracing.createLoggerFor(XlsMembersExport.class);
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
-	public MediaResource export(List<Identity> rows, Map<Identity, StringBuilder> members, Translator translator, List<UserPropertyHandler> userPropertyHandlers) {
-	
+public class XlsGroupMembersExport {
+
+	private static final OLog log = Tracing.createLoggerFor(XlsGroupMembersExport.class);
+	private String groupName;
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+
+	public MediaResource export(Map<String, GroupData> groups, Translator translator, List<UserPropertyHandler> userPropertyHandlers) {
+
 		String label = "TableExport_"
 				+ Formatter.formatDatetimeFilesystemSave(new Date(System.currentTimeMillis()))
 				+ ".xlsx";
-		
+
 		return new OpenXMLWorkbookResource(label) {
 			@Override
 			protected void generate(OutputStream out) {
 				try (OpenXMLWorkbook workbook = new OpenXMLWorkbook(out, 1)) {
 					OpenXMLWorksheet sheet = workbook.nextWorksheet();
 					createHeader(userPropertyHandlers, translator, sheet, workbook);
-					createData(members, rows, userPropertyHandlers, sheet);
+					for (String groupName : groups.keySet()) {
+						GroupData groupData = groups.get((String) groupName);
+						createData(groupData.getMembers(), groupData.getRows(), userPropertyHandlers, sheet);
+					}
 				} catch (IOException e) {
 					log.error("Unable to export xlsx", e);
 				}
@@ -69,29 +54,31 @@ public class XlsMembersExport {
 	}
 
 	protected void createHeader(List<UserPropertyHandler> userPropertyHandlers, Translator translator,
-			OpenXMLWorksheet sheet,	OpenXMLWorkbook workbook) {
-		Row headerRow = sheet.newRow();
+							  OpenXMLWorksheet sheet, OpenXMLWorkbook workbook) {
+		OpenXMLWorksheet.Row headerRow = sheet.newRow();
+		headerRow.addCell(0, translator.translate("form.name.group"), workbook.getStyles().getHeaderStyle());
 		for (int c = 0; c < userPropertyHandlers.size(); c++) {
-			// LMSUZH-566: Do not export email addresses even if email functionality is enabled in course element
 			if (!UserConstants.EMAIL.equals(userPropertyHandlers.get(c).getName())) {
 				UserPropertyHandler handler = userPropertyHandlers.get(c);
 				String header = translator.translate("form.name." + handler.getName());
-				headerRow.addCell(c, header, workbook.getStyles().getHeaderStyle());
+				headerRow.addCell(c + 1, header, workbook.getStyles().getHeaderStyle());
 			}
 		}
 		Translator roleTranslator = Util.createPackageTranslator(RulesDataModel.class, translator.getLocale());
 		headerRow.addCell(userPropertyHandlers.size(), roleTranslator.translate("rules.role"));
+		sheet.setHeaderRows(1);
 	}
 
-	protected void createData(Map<Identity, StringBuilder> members, List<Identity> rows, List<UserPropertyHandler> userPropertyHandlers, OpenXMLWorksheet sheet) {
-		sheet.setHeaderRows(1);
+	protected void createData(Map<Identity, StringBuilder> members, List<Identity> rows,
+							  List<UserPropertyHandler> userPropertyHandlers, OpenXMLWorksheet sheet) {
 		for (int r = 0; r < rows.size(); r++) {
-			Row dataRow = sheet.newRow();
+			OpenXMLWorksheet.Row dataRow = sheet.newRow();
+			dataRow.addCell(0, groupName, null);
 			for (int c = 0; c < userPropertyHandlers.size(); c++) {
 				// LMSUZH-566: Do not export email addresses even if email functionality is enabled in course element
 				if (!UserConstants.EMAIL.equals(userPropertyHandlers.get(c).getName())) {
 					String value = userPropertyHandlers.get(c).getUserProperty(rows.get(r).getUser(), null);
-					dataRow.addCell(c, value, null);
+					dataRow.addCell(c + 1, value, null);
 				}
 			}
 			dataRow.addCell(userPropertyHandlers.size(), members.get(rows.get(r)).toString());
