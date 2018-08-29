d4e30b7748983afac227299c1b851946877207e0
OLAT Team <team@olat.org>
2018-08-29 15:05:55 +0200

    tiny refactoring for reusing the courseEnvironment variable instead of repeating calls to a getter

--- src/main/java/org/olat/course/nodes/GTACourseNode.java
+++ src/main/java/org/olat/course/nodes/GTACourseNode.java
@@ -487,10 +487,11 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 	public CourseNode createInstanceForCopy(boolean isNewTitle, ICourse course, Identity author) {
 		GTACourseNode cNode = (GTACourseNode)super.createInstanceForCopy(isNewTitle, course, author);
 		GTAManager gtaManager = CoreSpringFactory.getImpl(GTAManager.class);
+		CourseEnvironment courseEnvironment = course.getCourseEnvironment();
 		
 		//copy tasks
-		File taskDirectory = gtaManager.getTasksDirectory(course.getCourseEnvironment(), this);
-		File copyTaskDirectory = gtaManager.getTasksDirectory(course.getCourseEnvironment(), cNode);
+		File taskDirectory = gtaManager.getTasksDirectory(courseEnvironment, this);
+		File copyTaskDirectory = gtaManager.getTasksDirectory(courseEnvironment, cNode);
 		FileUtils.copyDirContentsToDir(taskDirectory, copyTaskDirectory, false, "copy task course node");
 		
 		File taskDefinitions = new File(taskDirectory.getParentFile(), GTAManager.TASKS_DEFINITIONS);
@@ -500,8 +501,8 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 		}
 		
 		//copy solutions
-		File solutionsDirectory = gtaManager.getSolutionsDirectory(course.getCourseEnvironment(), this);
-		File copySolutionsDirectory = gtaManager.getSolutionsDirectory(course.getCourseEnvironment(), cNode);
+		File solutionsDirectory = gtaManager.getSolutionsDirectory(courseEnvironment, this);
+		File copySolutionsDirectory = gtaManager.getSolutionsDirectory(courseEnvironment, cNode);
 		FileUtils.copyDirContentsToDir(solutionsDirectory, copySolutionsDirectory, false, "copy task course node solutions");
 
 		File solutionDefinitions = new File(solutionsDirectory.getParentFile(), GTAManager.SOLUTIONS_DEFINITIONS);
@@ -517,6 +518,7 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 	public boolean archiveNodeData(Locale locale, ICourse course, ArchiveOptions options, ZipOutputStream exportStream, String charset) {
 		final GTAManager gtaManager = CoreSpringFactory.getImpl(GTAManager.class);
 		final ModuleConfiguration config =  getModuleConfiguration();
+		CourseEnvironment courseEnvironment = course.getCourseEnvironment();
 
 		String prefix;
 		if(GTAType.group.name().equals(config.getStringValue(GTACourseNode.GTASK_TYPE))) {
@@ -529,12 +531,12 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 				+ StringHelper.transformDisplayNameToFileSystemName(getShortName())
 				+ "_" + Formatter.formatDatetimeFilesystemSave(new Date(System.currentTimeMillis()));
 		
-		TaskList taskList = gtaManager.getTaskList(course.getCourseEnvironment().getCourseGroupManager().getCourseEntry(), this);
+		TaskList taskList = gtaManager.getTaskList(courseEnvironment.getCourseGroupManager().getCourseEntry(), this);
 
 		//save assessment datas
 		List<Identity> users = null;
 		if(config.getBooleanSafe(GTASK_GRADING)) {
-			users = ScoreAccountingHelper.loadUsers(course.getCourseEnvironment(), options);
+			users = ScoreAccountingHelper.loadUsers(courseEnvironment, options);
 			
 			String courseTitle = course.getCourseTitle();
 			String fileName = ExportUtil.createFileNameWithTimeStamp(courseTitle, "xlsx");
@@ -563,7 +565,7 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 				}
 			} else {
 				if(users == null) {
-					users = ScoreAccountingHelper.loadUsers(course.getCourseEnvironment(), options);
+					users = ScoreAccountingHelper.loadUsers(courseEnvironment, options);
 				}
 				
 				Set<Identity> uniqueUsers = new HashSet<>(users);
@@ -575,7 +577,7 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 
 		//copy solutions
 		if(config.getBooleanSafe(GTACourseNode.GTASK_SAMPLE_SOLUTION)) {
-			VFSContainer solutions = gtaManager.getSolutionsContainer(course.getCourseEnvironment(), this);
+			VFSContainer solutions = gtaManager.getSolutionsContainer(courseEnvironment, this);
 			if (solutions.exists()) {
 				String solutionDirName = dirName + "/solutions";
 				for(VFSItem solution:solutions.getItems(new SystemItemFilter())) {
@@ -590,6 +592,7 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 	private void archiveNodeData(ICourse course, Identity assessedIdentity, TaskList taskList, String dirName, ZipOutputStream exportStream) {
 		ModuleConfiguration config = getModuleConfiguration();
 		GTAManager gtaManager = CoreSpringFactory.getImpl(GTAManager.class);
+		CourseEnvironment courseEnvironment = course.getCourseEnvironment();
 		
 		int flow = 0;//for beautiful ordering
 		String userDirName = dirName + "/"
@@ -598,7 +601,7 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 		
 		Task task = gtaManager.getTask(assessedIdentity, taskList);
 		if(task != null && task.getTaskName() != null && config.getBooleanSafe(GTASK_ASSIGNMENT)) {
-			File taskDirectory = gtaManager.getTasksDirectory(course.getCourseEnvironment(), this);
+			File taskDirectory = gtaManager.getTasksDirectory(courseEnvironment, this);
 			File taskFile = new File(taskDirectory, task.getTaskName());
 			if(taskFile.exists()) {
 				String path = userDirName + "/"  + (++flow) + "_task/" + taskFile.getName(); 
@@ -607,13 +610,13 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 		}
 		
 		if(config.getBooleanSafe(GTASK_SUBMIT)) {
-			File submitDirectory = gtaManager.getSubmitDirectory(course.getCourseEnvironment(), this, assessedIdentity);
+			File submitDirectory = gtaManager.getSubmitDirectory(courseEnvironment, this, assessedIdentity);
 			String submissionDirName = userDirName + "/" + (++flow) + "_submissions";
 			ZipUtil.addDirectoryToZip(submitDirectory.toPath(), submissionDirName, exportStream);
 		}
 
 		if(config.getBooleanSafe(GTACourseNode.GTASK_REVIEW_AND_CORRECTION)) {
-			File correctionsDir = gtaManager.getCorrectionDirectory(course.getCourseEnvironment(), this, assessedIdentity);
+			File correctionsDir = gtaManager.getCorrectionDirectory(courseEnvironment, this, assessedIdentity);
 			String correctionDirName = userDirName + "/" + (++flow) + "_corrections";
 			ZipUtil.addDirectoryToZip(correctionsDir.toPath(), correctionDirName, exportStream);
 		}
@@ -621,11 +624,11 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 		if(task != null && config.getBooleanSafe(GTACourseNode.GTASK_REVISION_PERIOD)) {
 			int numOfIteration = task.getRevisionLoop();
 			for(int i=1; i<=numOfIteration; i++) {
-				File revisionDirectory = gtaManager.getRevisedDocumentsDirectory(course.getCourseEnvironment(), this, i, assessedIdentity);
+				File revisionDirectory = gtaManager.getRevisedDocumentsDirectory(courseEnvironment, this, i, assessedIdentity);
 				String revisionDirName = userDirName + "/" + (++flow) + "_revisions_" + i;
 				ZipUtil.addDirectoryToZip(revisionDirectory.toPath(), revisionDirName, exportStream);
 				
-				File correctionDirectory = gtaManager.getRevisedDocumentsCorrectionsDirectory(course.getCourseEnvironment(), this, i, assessedIdentity);
+				File correctionDirectory = gtaManager.getRevisedDocumentsCorrectionsDirectory(courseEnvironment, this, i, assessedIdentity);
 				String correctionDirName = userDirName + "/" + (++flow) + "_corrections_" + i;
 				ZipUtil.addDirectoryToZip(correctionDirectory.toPath(), correctionDirName, exportStream);
 			}
@@ -680,20 +683,21 @@ public class GTACourseNode extends AbstractAccessableCourseNode implements Persi
 	@Override
 	public void cleanupOnDelete(ICourse course) {
 		GTAManager gtaManager = CoreSpringFactory.getImpl(GTAManager.class);
+		CourseEnvironment courseEnvironment = course.getCourseEnvironment();
 		//tasks
-		File taskDirectory = gtaManager.getTasksDirectory(course.getCourseEnvironment(), this);
+		File taskDirectory = gtaManager.getTasksDirectory(courseEnvironment, this);
 		FileUtils.deleteDirsAndFiles(taskDirectory, true, true);
 		
 		//solutions
-		File solutionsDirectory = gtaManager.getSolutionsDirectory(course.getCourseEnvironment(), this);
+		File solutionsDirectory = gtaManager.getSolutionsDirectory(courseEnvironment, this);
 		FileUtils.deleteDirsAndFiles(solutionsDirectory, true, true);
 		
 		//clean up database
-		RepositoryEntry entry = course.getCourseEnvironment().getCourseGroupManager().getCourseEntry();
+		RepositoryEntry entry = courseEnvironment.getCourseGroupManager().getCourseEntry();
 		gtaManager.deleteTaskList(entry, this);
 		
 		//clean subscription
-		SubscriptionContext subscriptionContext = gtaManager.getSubscriptionContext(course.getCourseEnvironment(), this);
+		SubscriptionContext subscriptionContext = gtaManager.getSubscriptionContext(courseEnvironment, this);
 		NotificationsManager.getInstance().delete(subscriptionContext);
 	}
 
